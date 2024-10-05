787b2e9fccfb554eb9dc9c2150c06728d8c52c3d
Kity Dev <kitydeveloper@gmail.com>
2024-10-05 18:30:34 -0500

    salto de linea

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Escribe tu nombre:");
 var nombre = Console.ReadLine();
+
 Console.WriteLine("Nombre:" + nombre);
